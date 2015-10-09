28cc3368c53fb41166e78614d6d29a990fed048a
liumingzhij26 <liumingzhi@thefair.net.cn>
2015-10-09 18:28:41 +0800

    add

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,8 +1,6 @@
 <?php
 use Yaf\Dispatcher;
-use Services\TestService as Test;
 use Yaf\Controller_Abstract as Controller;
-use Yaf\Registry;
 
 class IndexController extends Controller
 {
@@ -30,37 +28,5 @@ class IndexController extends Controller
     {
         phpinfo();
     }
-
-    public function testAction()
-    {
-        $info['test'] = 0;
-        if (!empty($info['test'])) {
-            \Response\Response::Json($info);
-        }
-        print_r($info);
-    }
-
-    public function cacheAction()
-    {
-        $redis = new Redis();
-        $redis->connect('127.0.0.1');
-        echo $redis->set('page', '123123123');
-    }
-
-    public function getCacheAction()
-    {
-        $redis = new Redis();
-        $redis->connect('127.0.0.1');
-        $page = $redis->get('page');
-        print_r($page);
-    }
-
-    public function getdbAction()
-    {
-        $test = Test::Instance();
-        $setting = $test->setting();
-        \Response\Response::Json($setting);
-    }
-
 
 }
