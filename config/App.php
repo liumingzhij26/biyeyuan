89b42c5679cc8ba8fb1a362c1f49e6753bdb0c39
liumingzhij26 <liumingzhi@thefair.net.cn>
2015-11-28 14:02:01 +0800

    app

--- config/App.php
+++ config/App.php
@@ -10,41 +10,14 @@
  * Date: 14/11/24
  * Time: 下午6:54
  */
-namespace Config;
-class App
-{
+
+return [
+    'phase' => 'dev',   //string 开发环境pro
+
+    'platform' => 'h5',
+
+    'client_v' => '1.0',
 
 
-    /**
-     * @var string 开发环境
-     */
-    public $environment = 'dev';
 
-    public $platform = "pc";
-
-    public $version = '1.1';
-
-    public $cache = array(
-        //缓存类型
-        "type" => "file",
-        //缓存路径
-        "temp" => "/home/logs/file_cache/",
-        //缓存前缀
-        "prefix" => "",
-        //默认过期时间
-        "expire" => 1000,
-        //长度
-        "length" => 0,
-        //文件缓存子目录，为了防止单个目录下文件数过多,1为开启
-        "subdir" => 1,
-        //子目录缓存级别
-        "temp_level" => 1
-    );
-
-    public $api = array(
-        'static_image' => 'http://biyeyuan.com/',
-        'static_assets' => 'http://biyeyuan.com/',
-        'static_assets_date' => '201502021212',
-    );
-
-}
+];
